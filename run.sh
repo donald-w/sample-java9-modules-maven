0b30fb87295609c4a592b44c4c707e4ac1d47cb8
Donald Walker <github@donaldw.com>
2017-10-11 22:26:22 +0100

    Fix scripts

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 java \
 --module-path \
 jigsaw-service/target/jigsaw-service-0.0.1-SNAPSHOT.jar:\
-jigsaw/target/jigsaw-0.0.1-SNAPSHOT.jar:\
+jigsaw-app/target/jigsaw-app-0.0.1-SNAPSHOT.jar:\
 jigsaw-animals-service/target/jigsaw-animals-service-0.0.1-SNAPSHOT.jar:\
 jigsaw-numbers-service/target/jigsaw-numbers-service-0.0.1-SNAPSHOT.jar \
 -m \
